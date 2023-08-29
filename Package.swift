d1caf9414c763e65fc693c9fc16986b43fa301b5
Paul Popa <paul.popa@outlook.com>
2023-08-29 23:39:33 +0100

    Define platform and package dependencies

--- Package.swift
+++ Package.swift
@@ -5,24 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "FloatingButton",
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "FloatingButton",
-            targets: ["FloatingButton"]),
+        .library(name: "FloatingButton",targets: ["FloatingButton"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Themes", path: "../Themes"),
     ],
+    
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "FloatingButton",
-            dependencies: []),
-        .testTarget(
-            name: "FloatingButtonTests",
-            dependencies: ["FloatingButton"]),
+        .target(name: "FloatingButton", dependencies: [
+            
+            .productItem(name: "Themes", package: "Themes", condition: nil),
+        ]),
+        
+        .testTarget(name: "FloatingButtonTests", dependencies: ["FloatingButton"]),
     ]
 )
