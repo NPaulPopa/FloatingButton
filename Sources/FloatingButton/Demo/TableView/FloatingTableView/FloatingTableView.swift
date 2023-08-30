5f576d28ef96505dbec8e9cae77ad767cce299ee
Paul Popa <paul.popa@outlook.com>
2023-08-30 23:55:22 +0100

    Implement delegate methods for getting content offset and updating delegate

--- Sources/FloatingButton/Demo/TableView/FloatingTableView/FloatingTableView.swift
+++ Sources/FloatingButton/Demo/TableView/FloatingTableView/FloatingTableView.swift
@@ -30,4 +30,48 @@ public class FloatingButtonTableView: UITableView, UITableViewDelegate {
         configure()
     }
 
+    public required init?(coder: NSCoder) { fatalError("Not implemented") }
+    
+    //MARK: - View Configuration
+    
+    private func configure() {
+        registerCell()
+        configureDelegates()
+    }
+    
+    //MARK: - Delegates Configuration
+
+    private func configureDelegates() {
+        self.delegate = self
+        self.dataSource = floatingDataSource
+        self.delaysContentTouches = false
+    }
+    
+    private func registerCell() {
+        self.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+    }
+        
+    //MARK: - ScrollView Methods
+
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true) }
+
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let contentOffset = scrollView.contentOffset.y
+        
+        translation.send(contentOffset)
+        
+        if contentOffset > 0 {
+        
+        if contentOffset >= -64 {
+            floatingScrollDelegate?.updateFloatingButtonConstraints()
+        }
+            
+        }
+        
+        if contentOffset < -64 {
+           floatingScrollDelegate?.updateFloatingButtonConstraintsBackToNormal()
+        }
+    }
 }
