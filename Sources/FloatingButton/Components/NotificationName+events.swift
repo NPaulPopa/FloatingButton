5b7f2bc9a3d81c48b9f60bd3c6ae54721d818ecd
Paul Popa <paul.popa@outlook.com>
2023-09-07 23:54:16 +0100

    Add custom Notification name for adding new Items button being tapped

---
+++ Sources/FloatingButton/Components/NotificationName+events.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paul on 07/09/2023.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    
+    static let addItemsFloatingButtonTapped = Notification.Name("AddItemsFloatingButtonTappedNotification")
+
+}
