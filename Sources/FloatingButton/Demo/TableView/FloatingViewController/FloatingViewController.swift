fd1583679dfa9e11efe566a99b7349afd552e60d
Paul Popa <paul.popa@outlook.com>
2023-09-08 23:46:21 +0100

    Add protocol conformance and define button constraints used in protocol extension

--- Sources/FloatingButton/Demo/TableView/FloatingViewController/FloatingViewController.swift
+++ Sources/FloatingButton/Demo/TableView/FloatingViewController/FloatingViewController.swift
@@ -9,43 +9,34 @@ import UIKit
 import SwiftUI
 import Combine
 
-public class FloatingButtonViewController: UIViewController, FloatingScrollDelegate {
-    
-    var buttonWidthConstraint: NSLayoutConstraint!
-    var buttonHeightConstraint: NSLayoutConstraint!
+public class FloatingButtonViewController: UIViewController, FloatingButtonViewControllerProtocol {
         
+    //MARK: - Button Constraints
+    
+    public var buttonWidthConstraint: NSLayoutConstraint!
+    public var buttonHeightConstraint: NSLayoutConstraint!
+    public var buttonBottomConstraint: NSLayoutConstraint!
+   
     //MARK: - Properties
         
-    var translation = PassthroughSubject<CGFloat, Never>()
-    private var capsuleFloatingButton: UIView!
+    public var translationPublisher: PassthroughSubject<CGFloat, Never>!
+    public var swiftUICapsuleButton: FloatingButton!
+    public var capsuleFloatingButton: UIView!
     
-    var capsuleHeight: CGFloat!
-    var capsuleWidth: CGFloat!
+    public lazy var floatingTableView: FloatingButtonScrollViewProtocol = FloatingButtonTableView(translation: translationPublisher)
     
-    lazy var swiftUICapsuleButton = FloatingButton(translation: translation)
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+        setupButton()
+    }
     
-    lazy var floatingTableView = FloatingButtonTableView(translation: translation)
-    
-    private var capsuleCenterYConstraint: NSLayoutConstraint!
-    
-    public init() { super.init(nibName: nil, bundle: nil) }
     required init?(coder: NSCoder) { fatalError("not been implemented") }
     
     //MARK: - View DidLoad
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
-        floatingTableView.floatingScrollDelegate = self
-        
-        let hostingVC = UIHostingController(rootView: swiftUICapsuleButton)
-        self.capsuleFloatingButton = hostingVC.view
-        
-        self.capsuleWidth = swiftUICapsuleButton.capsuleWidth
-        self.capsuleHeight = swiftUICapsuleButton.capsuleHeight
-        
         constrainTableView()
-        constrainCapsuleFloatingButton()
     }
         
     //MARK: - Constraints
@@ -62,43 +53,5 @@ public class FloatingButtonViewController: UIViewController, FloatingScrollDeleg
             floatingTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             floatingTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
-    }
-    
-    func constrainCapsuleFloatingButton() {
-            
-        view.addSubview(capsuleFloatingButton)
-        
-        capsuleFloatingButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        capsuleCenterYConstraint = capsuleFloatingButton.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -60)
-
-        buttonWidthConstraint = capsuleFloatingButton.widthAnchor.constraint(equalToConstant: capsuleWidth)
-        
-        buttonHeightConstraint = capsuleFloatingButton.heightAnchor.constraint(equalToConstant: capsuleHeight)
-       
-        capsuleFloatingButton.layer.cornerRadius = capsuleHeight / 2
-        
-        NSLayoutConstraint.activate([
-                    
-            capsuleFloatingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            capsuleCenterYConstraint,
-            buttonWidthConstraint,
-            buttonHeightConstraint
-        ])
-    }
-    
-    //MARK: - Constraints Updating
-    
-    public func updateFloatingButtonConstraints() {
-        
-        buttonWidthConstraint.constant = 44.4
-      //  buttonHeightConstraint.constant = 44.4
-        self.capsuleFloatingButton.layer.cornerRadius = 44.4 / 2
-    }
-    
-    public func updateFloatingButtonConstraintsBackToNormal() {
-        
-        buttonWidthConstraint.constant = capsuleWidth
-      //  buttonHeightConstraint.constant = capsuleHeight
     }
 }
