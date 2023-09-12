f6f9cb4ebbc921082cd7801c12993c538fb64c71
Paul Popa <paul.popa@outlook.com>
2023-09-12 23:16:50 +0100

    Create custom notification name for sending contentOffset values

---
+++ Sources/FloatingButton/Demo/Extensions/NotificationName+translation.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paul on 12/09/2023.
+//
+
+import Foundation
+
+public extension Notification.Name {
+    
+    static let translationDidChange = Notification.Name("TranslationDidChangeNotification")
+}
